589f7ff0e84be796a726ec135f48f0676ceb8b61
Ankur <ankurr.singhal@gmail.com>
2014-01-27 21:44:15 +0530

    Completed 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,9 +16,9 @@ class User < ActiveRecord::Base
 
   has_attached_file :profile_pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
-  validates_attachment :profile_pic, :presence => true,
-  :content_type => { :content_type => ["image/jpeg","image/jpg","image/gif","image/png"] },
-  :size => { :in => 0..1.megabytes }
+  # validates_attachment :profile_pic, :presence => true,
+  # :content_type => { :content_type => ["image/jpeg","image/jpg","image/gif","image/png"] },
+  # :size => { :in => 0..1.megabytes }
 
   validates :username, :presence => true, :length => {:in => 8..16}, :uniqueness => { :case_sensitive => false }
   validates :first_name, :presence => true
